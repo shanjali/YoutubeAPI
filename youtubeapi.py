a5369c4454a3a5ec1fe39873ac9b35f9d8b60530
Anjali Sharma <anjalisharma@Anjalis-MacBook-Air-4.local>
2023-04-19 17:29:26 -0400

    focused on video stats

--- youtubeapi.py
+++ youtubeapi.py
@@ -14,30 +14,26 @@ def get_video_stats(ids):
 
     for i in range(0, len(ids), 25):
         request= youtube.videos().list(
-            part="snippet,contentDetails,statistics",
-            id=ids[i:i+50]
+            part="statistics",
+            id=ids[i:i+25]
         )
 
         data = request.execute()
         for video in data['items']:
-            title=video['snippet']['title']
-            published=video['snippet']['publishedAt']
-            description=video['snippet']['description']
-            tag_count= len(video['snippet']['tags'])
             view_count=video['statistics'].get('viewCount',0)
             like_count=video['statistics'].get('likeCount',0)
             dislike_count=video['statistics'].get('dislikeCount',0)
             comment_count=video['statistics'].get('commentCount',0)
-            stats_dict=dict(title=title, description=description, published=published, tag_count=tag_count, view_count=view_count, like_count=like_count, dislike_count=dislike_count, comment_count=comment_count)
+            stats_dict=dict(view_count=view_count, like_count=like_count, dislike_count=dislike_count, comment_count=comment_count)
             stats_list.append(stats_dict)
 
-        print(stats_list)
+    return stats_list
 
 
 def main():
     # test with despacito
     ids = ["kJQP7kiw5Fk"]
-    get_video_stats(ids)
+    print(get_video_stats(ids))
 
 if __name__ == "__main__":
     main()
